cbdafe8546a4ff2e449d9a82d6abbfec5de9df85
Sauon <25078006+Sauon@users.noreply.github.com>
2023-03-14 18:52:31 +0100

    Chore: Use common function for map and status commands

--- discord_bots/commands.py
+++ discord_bots/commands.py
@@ -995,6 +995,51 @@ def get_player_game(player_id: int, session=None) -> InProgressGame | None:
         return None
 
 
+def map_status_str(full_status: bool) -> str:
+    with Session() as session:
+        output = ""
+        current_map: CurrentMap | None = session.query(CurrentMap).first()
+        if current_map:
+            output += f"**Next map: {current_map.full_name} ({current_map.short_name})**\n"
+
+            if full_status:
+                rotation_maps: list[RotationMap] = session.query(RotationMap).order_by(RotationMap.created_at.asc()).all()  # type: ignore
+                next_rotation_map_index = (current_map.map_rotation_index + 1) % len(
+                    rotation_maps
+                )
+                next_map = rotation_maps[next_rotation_map_index]
+
+                time_since_update: timedelta = datetime.now(
+                    timezone.utc
+                ) - current_map.updated_at.replace(tzinfo=timezone.utc)
+                time_until_rotation = config.MAP_ROTATION_MINUTES - (
+                        time_since_update.seconds // 60
+                )
+                if config.RANDOM_MAP_ROTATION:
+                    output += f"_(Auto-rotates to a random map in {time_until_rotation} minutes)_\n"
+                else:
+                    if current_map.map_rotation_index == 0:
+                        output += f"_Map after next: {next_map.full_name} ({next_map.short_name})_\n"
+                    else:
+                        output += f"_Map after next (auto-rotates in {time_until_rotation} minutes): {next_map.full_name} ({next_map.short_name})_\n"
+                skip_map_votes: list[SkipMapVote] = session.query(SkipMapVote).all()
+                output += f"_Votes to skip (voteskip): [{len(skip_map_votes)}/{config.MAP_VOTE_THRESHOLD}]_\n"
+
+                map_votes: list[MapVote] = session.query(MapVote).all()
+                voted_map_ids: list[str] = [map_vote.voteable_map_id for map_vote in map_votes]
+                voted_maps: list[VoteableMap] = (
+                    session.query(VoteableMap).filter(VoteableMap.id.in_(voted_map_ids)).all()  # type: ignore
+                )
+                voted_maps_str = ", ".join(
+                    [
+                        f"{voted_map.short_name} [{voted_map_ids.count(voted_map.id)}/{config.MAP_VOTE_THRESHOLD}]"
+                        for voted_map in voted_maps
+                    ]
+                )
+                output += f"_Votes to change map (votemap): {voted_maps_str}_\n"
+        return output
+
+
 # Commands start here
 
 
@@ -2327,47 +2372,7 @@ async def trueskill(ctx: Context):
 
 @bot.command(name="map")
 async def map_(ctx: Context):
-    # TODO: This is duplicated
-    session = Session()
-    output = ""
-    current_map: CurrentMap | None = session.query(CurrentMap).first()
-    if current_map:
-        rotation_maps: list[RotationMap] = session.query(RotationMap).order_by(RotationMap.created_at.asc()).all()  # type: ignore
-        next_rotation_map_index = (current_map.map_rotation_index + 1) % len(
-            rotation_maps
-        )
-        next_map = rotation_maps[next_rotation_map_index]
-
-        time_since_update: timedelta = datetime.now(
-            timezone.utc
-        ) - current_map.updated_at.replace(tzinfo=timezone.utc)
-        time_until_rotation = config.MAP_ROTATION_MINUTES - (time_since_update.seconds // 60)
-        if config.RANDOM_MAP_ROTATION:
-            output += f"**Next map: {current_map.full_name} ({current_map.short_name})**\n_(Auto-rotates to a random map in {time_until_rotation} minutes)_\n"
-        else:
-            if current_map.map_rotation_index == 0:
-                output += f"**Next map: {current_map.full_name} ({current_map.short_name})**\n_Map after next: {next_map.full_name} ({next_map.short_name})_\n"
-            else:
-                output += f"**Next map: {current_map.full_name} ({current_map.short_name})**\n_Map after next (auto-rotates in {time_until_rotation} minutes): {next_map.full_name} ({next_map.short_name})_\n"
-    skip_map_votes: list[SkipMapVote] = session.query(SkipMapVote).all()
-    output += (
-        f"_Votes to skip (voteskip): [{len(skip_map_votes)}/{config.MAP_VOTE_THRESHOLD}]_\n"
-    )
-
-    # TODO: This is duplicated
-    map_votes: list[MapVote] = session.query(MapVote).all()
-    voted_map_ids: list[str] = [map_vote.voteable_map_id for map_vote in map_votes]
-    voted_maps: list[VoteableMap] = (
-        session.query(VoteableMap).filter(VoteableMap.id.in_(voted_map_ids)).all()  # type: ignore
-    )
-    voted_maps_str = ", ".join(
-        [
-            f"{voted_map.short_name} [{voted_map_ids.count(voted_map.id)}/{config.MAP_VOTE_THRESHOLD}]"
-            for voted_map in voted_maps
-        ]
-    )
-    output += f"_Votes to change map (votemap): {voted_maps_str}_\n\n"
-    session.close()
+    output = map_status_str(True)
     await ctx.send(embed=Embed(description=output, colour=Colour.blue()))
 
 
@@ -3090,48 +3095,8 @@ async def status(ctx: Context, *args):
             if game.queue_id:
                 games_by_queue[game.queue_id].append(game)
 
-        output = ""
-
-        current_map: CurrentMap | None = session.query(CurrentMap).first()
-        if current_map:
-            output += f"**Next map: {current_map.full_name} ({current_map.short_name})**\n"
-            # Only show full map info if they didn't request a specific queue
-            if full_status:
-                rotation_maps: list[RotationMap] = session.query(RotationMap).order_by(RotationMap.created_at.asc()).all()  # type: ignore
-                next_rotation_map_index = (current_map.map_rotation_index + 1) % len(
-                    rotation_maps
-                )
-                next_map = rotation_maps[next_rotation_map_index]
-
-                time_since_update: timedelta = datetime.now(
-                    timezone.utc
-                ) - current_map.updated_at.replace(tzinfo=timezone.utc)
-                time_until_rotation = config.MAP_ROTATION_MINUTES - (
-                    time_since_update.seconds // 60
-                )
-                if config.RANDOM_MAP_ROTATION:
-                    output += f"_(Auto-rotates to a random map in {time_until_rotation} minutes)_\n"
-                else:
-                    if current_map.map_rotation_index == 0:
-                        output += f"_Map after next: {next_map.full_name} ({next_map.short_name})_\n"
-                    else:
-                        output += f"_Map after next (auto-rotates in {time_until_rotation} minutes): {next_map.full_name} ({next_map.short_name})_\n"
-                skip_map_votes: list[SkipMapVote] = session.query(SkipMapVote).all()
-                output += f"_Votes to skip (voteskip): [{len(skip_map_votes)}/{config.MAP_VOTE_THRESHOLD}]_\n"
-
-                # TODO: This is duplicated
-                map_votes: list[MapVote] = session.query(MapVote).all()
-                voted_map_ids: list[str] = [map_vote.voteable_map_id for map_vote in map_votes]
-                voted_maps: list[VoteableMap] = (
-                    session.query(VoteableMap).filter(VoteableMap.id.in_(voted_map_ids)).all()  # type: ignore
-                )
-                voted_maps_str = ", ".join(
-                    [
-                        f"{voted_map.short_name} [{voted_map_ids.count(voted_map.id)}/{config.MAP_VOTE_THRESHOLD}]"
-                        for voted_map in voted_maps
-                    ]
-                )
-                output += f"_Votes to change map (votemap): {voted_maps_str}_\n\n"
+        output = map_status_str(full_status)
+        output += "\n"
 
         for i, queue in enumerate(queues):
             if i > 0 and full_status:
